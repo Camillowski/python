c84382aaf7a659dc4056d019bf547a419a7ce55f
Kamil Piasecki <kamil.piasecki@nokia.com>
2021-07-02 17:47:54 +0200

    Initial commit

---
+++ 1hour/main.py
@@ -0,0 +1,10 @@
+patient_name = "Kamil"
+patient_age = 20
+patient_isNew = True
+
+print ("We have a patient "+patient_name+" age: "+str(patient_age))
+print (f'We have a patient {patient_name} age: {patient_age}')
+
+my_name = input("Please enter your name: ")
+my_age = input("Please enter your age: ")
+print(f'My name is {my_name} and I am {my_age} years old')
